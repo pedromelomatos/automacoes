68904fd167f759bbaeb0fc0e8c24f1a9cc6d643c
Pedro Henrique Melo Matos <melomatosp@gmail.com>
2025-07-12 17:19:51 -0300

    automação finalizada

--- organizador_pastas/main.py
+++ organizador_pastas/main.py
@@ -12,3 +12,6 @@ for arquivo in arquivos_na_pasta:
     for tipo_de_arquivo in tipos:
         if tipo_de_arquivo in arquivo:
             print(f"{arquivo} é do tipo {tipo_de_arquivo}")
+            if not os.path.exists(f"{diretorio}/{tipo_de_arquivo}"): #se não tiver uma pasta pra esse arquivo..
+                os.mkdir(f"{diretorio}/{tipo_de_arquivo}") #crie ela
+            os.rename(f"{diretorio}/{arquivo}", f"{diretorio}/{tipo_de_arquivo}/{arquivo}") #mudando a localização do arquivo no os com os.rename
